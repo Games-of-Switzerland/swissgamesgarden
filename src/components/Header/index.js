5e4dee98119a31dac16b8742925a2ac3ba6fa628
Toni Fisler <mail@toni.io>
2019-12-20 11:34:06 +0100

    add a link around title in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import HeaderSearch from "./HeaderSearch";
 
 import './Header.scss';
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="header">
       <h1 className="header-title">
-        Swiss Games <br/>
-        Garden
+        <Link href="/">
+          <a>Swiss Games <br/>
+            Garden</a>
+        </Link>
       </h1>
       <HeaderSearch/>
       <div style={{color: 'var(--white)'}}>menu here</div>
