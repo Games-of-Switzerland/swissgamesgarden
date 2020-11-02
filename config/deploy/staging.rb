95ba70b13d79d5107f1ed72038ccab178c21accf
Kevin Wenger <wenger.kev@gmail.com>
2020-11-02 18:13:43 +0100

    deploy on dev instead of feature/deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@
 set :deploy_to, '/srv/staging-www.gos.ch'
 
 # set a branch for this release
-set :branch, 'feature/deploy'
+set :branch, 'dev'
 
 # Map docker and docker-compose commands
 # NOTE: If stage have different deploy_to
