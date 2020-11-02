38d55a22a8439112fd17bdc8e5ecc3f7ec75b694
Toni Fisler <mail@toni.io>
2020-11-02 22:07:33 +0100

    add Sentry

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,15 +4,23 @@ import 'styles/index.css';
 import {ReactQueryDevtools} from 'react-query-devtools';
 import 'locales/i18n';
 import Layout from 'components/Layout';
+import * as Sentry from '@sentry/node';
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    // enabled: process.env.NODE_ENV === 'production',
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  });
+}
 
 const queryCache = new QueryCache();
 
-export default function App({Component, pageProps}) {
+export default function App({Component, pageProps, err}) {
   return (
     <ReactQueryCacheProvider queryCache={queryCache}>
       <Hydrate state={pageProps.dehydratedState}>
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} err={err} />
         </Layout>
         <ReactQueryDevtools initialIsOpen />
       </Hydrate>
