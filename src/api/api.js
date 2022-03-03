e795532faee2bc5a48713c7cccacdb802e20a024
Toni Fisler <mail@toni.io>
2022-03-03 12:26:05 +0100

    setup elasticsearch prefetch url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 const fetchApi = async queryUrl => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_ELASTICSEARCH}/games?${queryUrl}`
+    `${typeof window === 'undefined'
+    ? process.env.NEXT_PUBLIC_SERVER_ELASTICSEARCH
+    : process.env.NEXT_PUBLIC_ELASTICSEARCH}/games?${queryUrl}`
   );
   return await res.json();
 };
