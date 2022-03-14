18e8baa82dfba0262947ba1fcc6ddc474a2708ed
Toni Fisler <mail@toni.io>
2022-03-14 08:59:54 +0100

    add title in bar

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {ReactQueryDevtools} from 'react-query/devtools';
 import {Hydrate} from 'react-query/hydration';
-import 'styles/index.css';
+import 'styles/index.css';import Head from 'next/head'
 
 const queryClient = new QueryClient();
 
@@ -13,11 +13,14 @@ export default function App({Component, pageProps}) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>Swiss Games Garden</title>
+        </Head>
         <DefaultSeo
           openGraph={{
             type: 'website',
             locale: 'en_CH',
-            site_name: 'Games of Switzerland',
+            site_name: 'Swiss Games Garden',
           }}
         />
         <Layout>
